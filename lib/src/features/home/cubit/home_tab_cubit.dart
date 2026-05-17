7761ebf0ed6ec113d54323681b991c77ddf929f8
admin <admin@gmail.com>
2026-05-17 15:12:08 +0100

    chore: remove Flutter mobile files to make the branch a clean standalone admin portal repository

--- lib/src/features/home/cubit/home_tab_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class HomeTabCubit extends Cubit<int> {
-  HomeTabCubit(super.initial);
-
-  void select(int index) {
-    if (state != index) emit(index);
-  }
-}
